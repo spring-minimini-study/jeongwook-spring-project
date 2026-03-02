a51a2effe389028c24beb72251cf4282decf6de9
Lee Jeong Wook <leejunguk14@naver.com>
2026-03-02 22:40:40 +0900

    spring init

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.board"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	//추가
+	implementation ("org.springframework.boot:spring-boot-starter-web") // RestController
+	implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly ("com.h2database:h2")
+	compileOnly ("org.projectlombok:lombok")
+	annotationProcessor ("org.projectlombok:lombok")
+
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "jeongwook-spring-project"
